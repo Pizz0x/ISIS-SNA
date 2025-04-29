a3c28ec709e757d57baf0d8dc55de60963c64406
Pizzox <filippopizzo03@gmail.com>
2025-04-29 11:32:57 +0200

    triadic closure, next step is to calculate the probability of a tc

--- ISIS_SNA.r
+++ ISIS_SNA.r
@@ -29,7 +29,7 @@ for (i in 1:nrow(df)) {
 
 mentions <- do.call(rbind, results) # transform the list of dataframe in a dataframe
 head(mentions)
-
+users <- unique(c(mentions$from, mentions$to)) # list of users mentioned in the edges (we need it later)
 # now we have our list of mentions, let's transform it into a weighted edge list:
 
 mention_counts <- as.data.frame(table(mentions$from, mentions$to), stringsAsFactors = FALSE)
@@ -67,51 +67,53 @@ dyad_census(net) # we can say that out network is definitely sparse
 
 
 # Let's get some more insight of the network, but first it's better to remove the nodes that do not appear in the edge table since they are not relevant for this part of the analysis
-net <- net - V(net)[degree(net, mode="all")==0]
-l <- layout.fruchterman.reingold(net)
-plot(net, edge.arrow.size=.1, edge.curved=.1, layout=l, vertex.label=NA) # in this way we also get a cleaner graph
-plot(net, edge.arrow.size=.1, edge.curved=.1, layout=layout_in_circle, vertex.label=NA)
-plot(net, edge.arrow.size=.1, edge.curved=.1, layout=layout_randomly, vertex.label=NA)
+net2 <- net - V(net)[degree(net, mode="all")==0]
+layout <- layout.fruchterman.reingold(net2)
+plot(net2, edge.arrow.size=.1, edge.curved=.1, layout=layout, vertex.label=NA) # in this way we also get a cleaner graph
+plot(net2, edge.arrow.size=.1, edge.curved=.1, layout=layout_in_circle, vertex.label=NA)
+plot(net2, edge.arrow.size=.1, edge.curved=.1, layout=layout_randomly, vertex.label=NA)
 
 
 # since our network is basically a citation network, we can measure the centrality of the graph and see the most important vertex of the graph
 # we will at first measure eigen centrality, that is a refinement that assigns higher weight to vertex for being connected to vertices which are themselves important
-eigenCent <- eigen_centrality(net)$vector
+eigenCent <- eigen_centrality(net2)$vector
 # let's plot the score on the graph:
 bins <- unique(quantile(eigenCent, seq(0,1,length.out=15)))
 vals <- cut(eigenCent, bins, labels=FALSE, include.lowest=TRUE)
 my_col = heat.colors(length(bins))
 colorVals <- rev(my_col)[vals]
-V(net)$color <- colorVals
-plot(net, edge.arrow.size=.1, edge.curved=.1, layout=l, vertex.label=NA)
+V(net2)$color <- colorVals
+plot(net2, edge.arrow.size=.1, edge.curved=.1, layout=layout, vertex.label=NA)
 # so who are the more relevant user?
 sort(eigenCent,decreasing=TRUE)[1:10]
 
 # we will now measure betweenness centrality, to see which are the vertices that connect important parts of the graph
-betweenCent <- betweenness(net)
+betweenCent <- betweenness(net2)
 cor(betweenCent,eigenCent)
 bins <- unique(quantile(betweenCent, seq(0,1,length.out=30)))
 vals <- cut(betweenCent, bins, labels=FALSE, include.lowest=TRUE)
 colorVals <- rev(heat.colors(length(bins)))[vals]
-V(net)$color <- colorVals
-plot(net, edge.arrow.size=.1, edge.curved=.1, layout=l, vertex.label=NA)
+V(net2)$color <- colorVals
+plot(net2, edge.arrow.size=.1, edge.curved=.1, layout=layout, vertex.label=NA)
 # Which are the most useful users to the graph connection?
 sort(betweenCent,decreasing=TRUE)[1:10]
 
 # let's now try to detect the communities -> disconnected parts of the graph
-w <- cluster_edge_betweenness(net)
+w <- cluster_edge_betweenness(net2)
 sort(table(w$membership))
 # we have 5 significant communities with 9 or more nodes, let's remake the graph with different color for each community
-V(net)$color <- rep("white", length(w$membership))
+V(net2)$color <- rep("white", length(w$membership))
 keepTheseCommunities <- names(sizes(w))[sizes(w) > 4]
 matchIndex <- match(w$membership, keepTheseCommunities) # like %in%
 colorVals <- rainbow(10)[matchIndex[!is.na(matchIndex)]]
-V(net)$color[!is.na(matchIndex)] <- colorVals
-plot.igraph(net, vertex.label = NA,layout=l, vertex.size=5)
+V(net2)$color[!is.na(matchIndex)] <- colorVals
+plot.igraph(net2, vertex.label = NA,layout=layout, vertex.size=5)
 
 
 
-# we now want to do a temporal analysis on the connections, specifically we want to check for triadic closures
+# we now want to do a temporal analysis on the connections, specifically we want to check for triadic closures:
+# if A is friend with B and B is friend with C, then A tend to become friend with C
+# we want to calculate the number of triangles that will create over time and how many open triads become close triads
 
 # first we need to sort the data set based on the timestamp:
 class(df$time) # we want a datatime value not character
@@ -121,13 +123,15 @@ sorted_df <- df[order(df$time),]
 
 # now we want to create the graph progressively by the time, the idea is, since the graph is quite sparse, to see how it evolve month by month
 df$month <- format(df$time, "%Y-%m") # to do this we need a new column -> month
-net <- make_empty_graph(directed=F) # start from an empty graph
+net3 <- make_empty_graph(directed=F) # start from an empty graph
+net3 <- add_vertices(net3, length(users), name=users)
+#layout <- layout_in_circle(net)
 # and for each month, we add the edges for the monthly tweets, measure the number of triadic closure and update the graph
 triangles_over_time <- c()
 months <- sort(unique(df$month))
-months <- months[-c(1,2,3,4,5)]
+months <- months[-c(1,2,3,4,5,6,7,8)]
 months
-par(mfrow=c(3,4))
+par(mfrow=c(3,3))
 
 for (m in months){
   df_month <- df[df$month == m, ]
@@ -150,23 +154,29 @@ for (m in months){
   }
   if(length(edges) > 0){
     new_edges <- matrix(edges, ncol=2, byrow=T)
-    new_nodes <- setdiff(unique(c(new_edges)), V(net)$name)
-    if (length(new_nodes) > 0) {
-      net <- add_vertices(net, length(new_nodes), name=new_nodes)
-    }
-    net <- add_edges(net, t(new_edges))
+    #new_nodes <- setdiff(unique(c(new_edges)), V(net)$name)
+    #if (length(new_nodes) > 0) {
+    #  net <- add_vertices(net, length(new_nodes), name=new_nodes)
+    #}
+    net3 <- add_edges(net3, t(new_edges))
   }
+  triangles <- sum(count_triangles(net3))/3
+  triangles_over_time <- c(triangles_over_time, triangles)
+  net3 <- simplify(net3, remove.multiple = TRUE, remove.loops=T)
   plot(
-    net, 
+    net3, 
     main=paste("Rete al mese:", m),
-    layout=layout_with_fr(net),
+    layout=layout,
     vertex.size=3, 
     vertex.label=NA,
     edge.arrow.size=.2
   )
 }
 
+par(mfrow = c(1, 1))
 
+plot(months, triangles_over_time, type = "b", col = "blue",
+     xlab = "Mese", ylab = "Triangoli (Triadic Closure)", main = "Evoluzione del Triadic Closure")
 
 
 # we are now interested in the most used words in the tweets of the ISIS fan, so from the starting dataset, we are interested in the fields tweet
